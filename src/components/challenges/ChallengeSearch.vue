d2aff9323ab441b5b6a91f7ebd7082f5aff0aecb
김정훈 <junghun2581@naver.com>
2023-05-23 09:06:05 +0900

    before merge

---
+++ src/components/challenges/ChallengeSearch.vue
@@ -0,0 +1,31 @@
+<!-- 챌린지 검색 -->
+<template>
+  <div>
+    <h2>챌린지 검색</h2>
+  </div>
+</template>
+<script>
+import { mapGetters } from "vuex";
+import ListRow from "@/components/challenges/include/ListRow.vue";
+import reListRow from "@/components/challenges/include/reListRow.vue";
+
+export default {
+  name: "challengeList",
+  components: {
+    ListRow,
+    reListRow,
+  },
+  computed: {
+    ...mapGetters(["challenges", "challengesSort"]),
+  },
+  created() {
+    this.$store.dispatch("getChallenges");
+    this.$store.dispatch("getChallengesSort", {
+      orderBy: "likeCnt",
+      orderByDir: "desc",
+    });
+  },
+};
+</script>
+
+<style></style>
